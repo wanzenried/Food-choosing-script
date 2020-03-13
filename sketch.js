3def76227bb4344ca0b40c7db690eba3a7706909
wanzenried <wanzenried@users.noreply.github.com>
2020-03-13 13:38:54 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+
+
 function setup() {
   // put setup code here
+  createCanvas(600,600);
+  // loadFile("retter-liste.txt")
+  console.log((await loadFile("retter-liste.txt")));
 }
 
 function draw() {
   // put drawing code here
+}
+
+function loadFile(path) {
+  let d1;
+   fetch(path).then((response) => {
+    return response.text();
+
+  }).then((data) => {
+    console.log(typeof(data));
+  })
+  return d1;
+}
+
+function convertToArray(file) {
+
 }
